0662ee04d2c3ec2da11840268b3fa920a7c719f6
Jesus Peralta <jesus_web_master@hotmail.com>
2025-06-12 14:24:41 -0600

    React base

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+function App() {
+
+  return (
+    <>
+     <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-white text-center font-black text-4xl">
+          Planificador de Gastos
+        </h1>
+     </header>
+
+     <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+      <p>FORMULARIO AQUI</p>
+     </div>
+    </>
+  )
+}
+
+export default App
