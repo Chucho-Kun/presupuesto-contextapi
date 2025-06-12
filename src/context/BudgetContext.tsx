718be96ceb4c21b1330167e267088067f93d7adc
Jesus Peralta <jesus_web_master@hotmail.com>
2025-06-12 16:51:01 -0600

    contextAPI defines

---
+++ src/context/BudgetContext.tsx
@@ -0,0 +1,29 @@
+import { useReducer , createContext , Dispatch } from "react"
+import { budgetReducer , initialState, type BudgetActions } from "../reducers/budget-reducer"
+
+type BudgetContextProps = {
+    state:BudgetState
+    dispatch: Dispatch<BudgetActions>
+}
+
+type BudgetProviderProps = {
+    children: ReactNode
+}
+
+export const BudgetContext  = createContext<BudgetContextProps>(null!)
+
+export const BudgetProvider = ({ children }) => {
+
+const [ state , dispatch ] = useReducer( budgetReducer , initialState )
+
+    return(
+        <BudgetContext.Provider
+            value={{
+                state,
+                dispatch
+            }}
+        >
+            { children }
+        </BudgetContext.Provider>
+    )
+}
