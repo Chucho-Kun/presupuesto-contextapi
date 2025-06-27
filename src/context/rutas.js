da30e3ce034af1ad43e08eeafda816bb8a0ab17c
Jesus Peralta <jesus_web_master@hotmail.com>
2025-06-26 18:21:34 -0600

    screenshot published

---
+++ src/context/rutas.js
@@ -0,0 +1,73 @@
+// Respuesta para 1 viaje redondo
+
+let response = {
+    "bulk_trip_options_result":{
+        "itinerary_responses":[{
+            "itinerary":{
+                "segment_keys": [
+                    {
+                    "ticketing_trip_id": "123456",
+                    "from_ticketing_stop_time_id": "ZRH-1234",
+                    "to_ticketing_stop_time_id": "LUZ-1235",
+                    "service_date":{
+                        "year": 2023,
+                        "month": 5,
+                        "day": 15
+                    },
+                    "boarding_time": {
+                    "year": 2023,
+                    "month": 5,
+                    "day": 15,
+                    "hours": 14,
+                    "minutes": 25,
+                    "seconds": 0,
+                    "nanos": 0,
+                    "utc_offset": "0s"
+                },
+                "arrival_time":{
+                    "year": 2023,
+                    "month": 5,
+                    "day": 15,
+                    "hours": 16,
+                    "minutes": 25,
+                    "seconds": 0,
+                    "nanos": 0,
+                    "utc_offset": "0s"
+                }
+            },
+            {
+                "ticketing_trip_id": "988833",
+                "from_ticketing_stop_time_id": "LUZ-1235",
+                "to_ticketing_stop_time_id": "WOL-2455",
+                "service_date": {
+                    "year": 2023,
+                    "month": 5,
+                    "day": 15
+              },
+              "boarding_time": {
+                "year": 2023,
+                "month": 5,
+                "day": 15,
+                "hours": 18,
+                "minutes": 13,
+                "seconds": 0,
+                "nanos": 0,
+                "utc_offset": "0s"
+              },
+              "arrival_time": {
+                "year": 2023,
+                "month": 5,
+                "day": 15,
+                "hours": 20,
+                "minutes": 13,
+                "seconds": 0,
+                "nanos": 0,
+                "utc_offset": "0s"
+              }
+            }]
+        }
+
+
+        }]
+    }
+}
